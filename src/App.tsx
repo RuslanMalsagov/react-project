389625db8807a62ae5d8d533bd516be3572c9f0e
Malsagov <rus.malsagov177@mail.ru>
2023-12-06 23:02:24 +0300

    Добавил папку и файл classNames, и функцию classNames для добавления классов

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './styles/index.scss';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/aboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/mainPage/MainPage.async';
-import { Suspense, useContext, useState } from 'react';
-import { Theme, ThemeContext } from './theme/ThemeContext';
+import { Suspense } from 'react';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toogleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toogleTheme}>Theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
