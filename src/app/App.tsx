fb6a8c40ff1979df6be3efc6698074d78a60d8d1
Malsagov <rus.malsagov177@mail.ru>
2023-12-07 23:25:30 +0300

    Изменил структуру проекта и настроил абсолютные импорты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { Link } from 'react-router-dom';
-import { AboutPageAsync } from './pages/aboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/mainPage/MainPage.async';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
   const { theme, toogleTheme } = useTheme();
@@ -16,8 +16,8 @@ const App = () => {
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />} />
-          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
--- webpack.config.ts
+++ webpack.config.ts
@@ -8,6 +8,8 @@ export default (env: BuildEnv) => {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
+    //src - путь до папки с исходным кодом
+    src: path.resolve(__dirname, 'src'),
   };
 
   const mode = env.mode || 'development';
