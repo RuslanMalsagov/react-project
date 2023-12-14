9e5d0c2066d448ee3cbac9f787e0a92bc3081266
Malsagov <rus.malsagov177@mail.ru>
2023-12-14 22:41:19 +0300

    Добавил переводы и разбил их на асинхронные чанки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,22 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
+import { Suspense } from 'react';
 
 const App = () => {
   const { theme } = useTheme();
+
+  // Suspense fallback='' - мы используем для i18n
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
