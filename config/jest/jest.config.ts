85812f97148a67d2f4329bd1f3aa1cc147432237
Malsagov <rus.malsagov177@mail.ru>
2024-01-02 15:36:28 +0300

    jest.config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,26 +1,13 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/vg/885sgz753b7_ndm_ptktf6rc0000gn/T/jest_dx",
-
-  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: ['/node_modules/'],
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+  modulePaths: ['<rootDir>src'],
   rootDir: '../../',
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
